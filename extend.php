2b388eb63fc4fa9cd55a73837a5b84316af8b9b5
Lady-Byron <samantha-everett@outlook.com>
2025-10-31 17:10:35 -0400

    Update extend.php

--- extend.php
+++ extend.php
@@ -5,4 +5,8 @@ return [
     (new Extend\Routes('api'))
         ->get('/arrowchat/whoami', 'lady-byron.arrowchat.whoami', \LadyByron\ArrowChat\Controllers\WhoAmIController::class)
         ->get('/arrowchat/users/{id:[0-9]+}', 'lady-byron.arrowchat.user', \LadyByron\ArrowChat\Controllers\UserInfoController::class),
+
+    // ★ 新增：在论坛请求管线里加入我们的心跳中间件
+    (new Extend\Middleware('forum'))
+        ->add(\LadyByron\ArrowChat\Middleware\Heartbeat::class),
 ];
